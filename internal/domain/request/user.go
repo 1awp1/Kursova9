5f8cf2012befe4d62d801f91e146b962b5c6883c
1awp1 <loktevdiman@mail.ru>
2024-11-27 01:42:24 +0400

    feat: added new page for user and admin

--- internal/domain/request/user.go
+++ internal/domain/request/user.go
@@ -1,10 +1,10 @@
 package request
 
-type UpdateUser struct { //todo
-	FirstName *string
-	LastName  *string
-	Login     *string
-	Phome     *string
-	Email     *string
-	Password  *string
+type UpdateUser struct {
+	FirstName *string `json:"first_name" form:"first_name" binding:"required"`
+	LastName  *string `json:"last_name" form:"last_name" binding:"required"`
+	Login     *string `json:"login" form:"login" binding:"required"`
+	Phone     *string `json:"nhone" form:"phome" binding:"required"`
+	Email     *string `json:"email" form:"email" binding:"required"`
+	Password  *string `json:"password " form:"password " binding:"required"`
 }
